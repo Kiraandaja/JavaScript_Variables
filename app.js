7b0ffde921e3f827b7ac2990684abc1b3dfea554
Kiraandaja <103534347+Kiraandaja@users.noreply.github.com>
2022-06-14 18:44:40 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,8 +3,36 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 
+const firstName= "Takira";
+let lastName = "Gray";
+let cityName = "Baltimore"; 
+let pastTime= " That's So Raven";
+var myAge = 28;
+//Exercise 2
+let fullName = `$ {firstName} {lastName};`
+console.log("fullName");
 
-// Exercise 2
+//Exercise 3
+let myStory = "My story is a simple, yet complex one. I have experienced many aspects of life. Ultimately, I am grateful.";
+let introduction = `$This is my introduction. As a reminder, my name is {firstName} {lastName}. I am from {cityName}. My favortie pastime growing up was {pastTime}` ;
+console.log ("myStory");
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = "1995";
+let isCaseSensitive = "true";
+//Exercise 4
+let price = "19.99";
+let isOnSale= "false";
+let salePercentage = "15";
+let stock = "0";
+let inStock = "false";
+let selectedSize = "M";
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = "722";
+let bookmark = "456";
+let hasRead = "true";
+
